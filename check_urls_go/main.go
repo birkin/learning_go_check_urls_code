85614e30bfca97226be3a6b4f9bf4fea2d2d2e88
birkin <birkin.diana@gmail.com>
2017-08-31 08:24:33 -0400

    adds urls and experiments with printing.

--- check_urls_go/main.go
+++ check_urls_go/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+    "github.com/davecgh/go-spew/spew"
 )
 
 type Site struct {
@@ -32,9 +33,29 @@ func main() {
 		Site{"callnumber_app",
 			"https://apps.library.brown.edu/callnumber/v2/?callnumber=PS3576",
 			"American Literature"},
+		Site{"clusters api",
+			"https://library.brown.edu/clusters_api/data/",
+			"scili-friedman"},
+		Site{"easyborrow_feed",
+			"http://library.brown.edu/easyborrow/feeds/latest_items/",
+			"easyBorrow -- recent requests"},
+		Site{"freecite",
+			"http://freecite.library.brown.edu/welcome/",
+			"About FreeCite"},
+		Site{"iip_inscriptions",
+			"http://library.brown.edu/cds/projects/iip/viewinscr/abur0001/",
+			"Khirbet Abu Rish"},
+		Site{"iip_processor",
+			"https://apps.library.brown.edu/iip_processor/info/",
+			"hi"},
 	}
 
 	fmt.Println(sites)
-	fmt.Println(sites[0])
+    fmt.Println( "---" )
+	// fmt.Println(sites[0])
+	fmt.Printf("%+v\n", sites) // adding `%+v` prints the field-names
+    fmt.Println( "---" )
+    spew.Dump(sites)
+    fmt.Println( "---" )
 
 } // end func main()
