86415285659facda1314feafaa92c2c35dd25120
birkin <birkin.diana@gmail.com>
2017-09-17 08:25:39 -0400

    updates vars.

--- check_urls_go/main.go
+++ check_urls_go/main.go
@@ -71,38 +71,38 @@ func initialize_sites() []Site {
 	sites = []Site{}
 	sites = append(
 		sites,
-		// Site{
-		// 	label:    "repo_file",
-		// 	url:      "https://repository.library.brown.edu/storage/bdr:6758/PDF/",
-		// 	expected: "BleedBox", // note: since brace is on following line, this comma is required
-		// },
-		// Site{"repo_search",
-		// 	"https://repository.library.brown.edu/studio/search/?q=elliptic",
-		// 	"The sequence of division polynomials"},
-		// Site{"bipg_wiki",
-		// 	"https://wiki.brown.edu/confluence/display/bipg/Brown+Internet+Programming+Group+Home",
-		// 	"The BIPG idea"},
-		// Site{"booklocator_app",
-		// 	"http://library.brown.edu/services/book_locator/?callnumber=GC97+.C46&location=sci&title=Chemistry+and+biochemistry+of+estuaries&status=AVAILABLE&oclc_number=05831908&public=true",
-		// 	"GC97 .C46 Level 11, Aisle 2A"},
-		// Site{"callnumber_app",
-		// 	"https://apps.library.brown.edu/callnumber/v2/?callnumber=PS3576",
-		// 	"American Literature"},
+		Site{
+			label:    "repo_file",
+			url:      "https://repository.library.brown.edu/storage/bdr:6758/PDF/",
+			expected: "BleedBox", // note: since brace is on following line, this comma is required
+		},
+		Site{"repo_search",
+			"https://repository.library.brown.edu/studio/search/?q=elliptic",
+			"The sequence of division polynomials"},
+		Site{"bipg_wiki",
+			"https://wiki.brown.edu/confluence/display/bipg/Brown+Internet+Programming+Group+Home",
+			"The BIPG idea"},
+		Site{"booklocator_app",
+			"http://library.brown.edu/services/book_locator/?callnumber=GC97+.C46&location=sci&title=Chemistry+and+biochemistry+of+estuaries&status=AVAILABLE&oclc_number=05831908&public=true",
+			"GC97 .C46 Level 11, Aisle 2A"},
+		Site{"callnumber_app",
+			"https://apps.library.brown.edu/callnumber/v2/?callnumber=PS3576",
+			"American Literature"},
 		Site{"clusters api",
 			"https://library.brown.edu/clusters_api/data/",
 			"scili-friedman"},
-		// Site{"easyborrow_feed",
-		// 	"http://library.brown.edu/easyborrow/feeds/latest_items/",
-		// 	"easyBorrow -- recent requests"},
+		Site{"easyborrow_feed",
+			"http://library.brown.edu/easyborrow/feeds/latest_items/",
+			"easyBorrow -- recent requests"},
 		Site{"freecite",
 			"http://freecite.library.brown.edu/welcome/",
 			"About FreeCite"},
-		// Site{"iip_inscriptions",
-		// 	"http://library.brown.edu/cds/projects/iip/viewinscr/abur0001/",
-		// 	"Khirbet Abu Rish"},
-		// Site{"iip_processor",
-		// 	"https://apps.library.brown.edu/iip_processor/info/",
-		// 	"hi"},
+		Site{"iip_inscriptions",
+			"http://library.brown.edu/cds/projects/iip/viewinscr/abur0001/",
+			"Khirbet Abu Rish"},
+		Site{"iip_processor",
+			"https://apps.library.brown.edu/iip_processor/info/",
+			"hi"},
 		Site{"not_found_test",
 			"https://apps.library.brown.edu/iip_processor/info/",
 			"foo"},
@@ -114,6 +114,8 @@ func initialize_sites() []Site {
 
 func check_sites_with_goroutines(sites []Site) {
 	/* Creates channel, kicks off go-routines, prints channel output, and closes channel. */
+
+	main_start := time.Now()
 
 	/// initialize channel
 	writer_channel := make(chan Result)
@@ -137,13 +139,16 @@ func check_sites_with_goroutines(sites []Site) {
 			break // shouldn't be needed
 		}
 	}
+	main_elapsed := time.Since(main_start)
+	rlog.Info(fmt.Sprintf("main_elapsed, ```%v```", main_elapsed) )
+
 }
 
 func check_site(site Site, writer_channel chan Result) {
 	/* Checks site, stores data to result, & writes info to channel. */
 
 	/// check site
-	start := time.Now()
+	mini_start := time.Now()
 	resp, _ := http.Get(site.url)
 	body_bytes, _ := ioutil.ReadAll(resp.Body)
 	text := string(body_bytes)
@@ -153,11 +158,11 @@ func check_site(site Site, writer_channel chan Result) {
 	}
 
 	/// store result
-	elapsed := time.Since(start)
+	mini_elapsed := time.Since(mini_start)
 	result_instance := Result{
 		label:        site.label,
 		check_result: site_check_result,
-		time_taken:   elapsed,
+		time_taken:   mini_elapsed,
 	}
 
 	/// write info to channel
